185c82b4d8430690e81fe3b6c20c16cb9a9ba48d
smithjke <smithjke@gmail.com>
2022-10-21 17:46:31 +0300

    fix: add get plugins function to releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,59 @@
 const ref = process.env.GITHUB_REF;
 const branch = ref.split('/').pop();
 
+function getPlugins() {
+  if (branch === 'master') {
+    return [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/npm',
+        {
+          npmPublish: false,
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: [
+            'CHANGELOG.md',
+            'package.json',
+          ],
+          message: '${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+      '@semantic-release/github',
+    ];
+  }
+
+  return [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'package.json',
+        ],
+        message: '${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ];
+}
+
 module.exports = {
   branches: [
     'master',
@@ -14,31 +67,5 @@ module.exports = {
       prerelease: true,
     },
   ],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: `CHANGELOG_${branch}_.md`,
-      },
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: [
-          'CHANGELOG.md',
-          'package.json',
-        ],
-        message: '${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-    '@semantic-release/github',
-  ],
+  plugins: getPlugins(),
 }
